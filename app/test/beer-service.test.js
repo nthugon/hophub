ac002e4d8b08e6f154d7b4b9aab71d24d0a3ee48
Nathan Hugon <nthugon@gmail.com>
2018-08-08 11:17:02 -0700

    added test to beer-service for getByUser

--- app/test/beer-service.test.js
+++ app/test/beer-service.test.js
@@ -6,10 +6,65 @@ describe('beer service', () => {
     before(angular.mock.module('services', { apiUrl: '/api' }));
     
     let $httpBackend = null, beerService = null;
+
+    const beer1 = {
+        abv: 5,
+        brewery: 'some brewery',
+        enteredBy: 'some user',
+        name: 'beer name',
+        style: 'beer style',
+        userId: 123,
+        __v: 0,
+        _id: 123
+    };
+
+    const beer2 = {
+        abv: 5,
+        brewery: 'some other brewery',
+        enteredBy: 'some other user',
+        name: 'other beer name',
+        style: 'other beer style',
+        userId: 246,
+        __v: 0,
+        _id: 456
+    };
+
+    const beer3 = {
+        abv: 5,
+        brewery: 'another brewery',
+        enteredBy: 'another user',
+        name: 'another beer name',
+        style: 'another beer style',
+        userId: 246,
+        __v: 0,
+        _id: 789
+    };
+
+    const beer4 = {
+        abv: 5,
+        brewery: 'yet another brewery',
+        enteredBy: 'yet another user',
+        name: 'yet another beer name',
+        style: 'yet another beer style',
+        userId: 135,
+        __v: 0,
+        _id: 135
+    };
+
+    const user = {
+        admin: false,
+        brewer: false,
+        username: 'nathan',
+        _id: 246
+    };
+
+    const beers = [beer1, beer2, beer3];
+    const usersBeers = [beer2, beer3];
     
     before(angular.mock.inject((_beerService_, _$httpBackend_ ) => {
         $httpBackend = _$httpBackend_;
         beerService = _beerService_;
+
     }));
 
     afterEach(() => {
@@ -18,41 +73,7 @@ describe('beer service', () => {
     });
 
     it('gets beers', done => {
-        const beer1 = {
-            abv: 5,
-            brewery: 'some brewery',
-            enteredBy: 'some user',
-            name: 'beer name',
-            style: 'beer style',
-            userId: 123,
-            __v: 0,
-            _id: 123
-        };
-
-        const beer2 = {
-            abv: 5,
-            brewery: 'some other brewery',
-            enteredBy: 'some other user',
-            name: 'other beer name',
-            style: 'other beer style',
-            userId: 456,
-            __v: 0,
-            _id: 456
-        };
-
-        const beer3 = {
-            abv: 5,
-            brewery: 'another brewery',
-            enteredBy: 'another user',
-            name: 'another beer name',
-            style: 'another beer style',
-            userId: 789,
-            __v: 0,
-            _id: 789
-        };
-
-        const beers = [beer1, beer2, beer3];
-        
+       
         $httpBackend
             .expectGET('/api/beers')
             .respond(beers);
@@ -67,18 +88,24 @@ describe('beer service', () => {
         $httpBackend.flush();
     });
 
+    it('gets beers by user', done => {
+       
+        $httpBackend
+            .expectGET('/api/beers/user/246')
+            .respond(usersBeers);
+
+        beerService.getByUser(user._id)
+            .then(beersByUserId => {
+                assert.deepEqual(beersByUserId, usersBeers);
+                done();
+            })
+            .catch(done);
+
+        $httpBackend.flush();
+    });
+
     it('adds a beer', done => {
-        const beer4 = {
-            abv: 5,
-            brewery: 'yet another brewery',
-            enteredBy: 'yet another user',
-            name: 'yet another beer name',
-            style: 'yet another beer style',
-            userId: 135,
-            __v: 0,
-            _id: 135
-        };
-  
+ 
         $httpBackend
             .expectPOST('/api/beers', beer4)
             .respond(beer4);
@@ -94,24 +121,14 @@ describe('beer service', () => {
     });
 
     it('deletes a beer', done => {
-        const beer5 = {
-            abv: 5,
-            brewery: 'still another brewery',
-            enteredBy: 'still another user',
-            name: 'still another beer name',
-            style: 'still another beer style',
-            userId: 246,
-            __v: 0,
-            _id: 246
-        };
         
         $httpBackend
-            .expectDELETE('/api/beers/246')
-            .respond(beer5);
+            .expectDELETE('/api/beers/135')
+            .respond(beer4);
 
-        beerService.remove(beer5._id)
+        beerService.remove(beer4._id)
             .then(deletedBeer => {
-                assert.deepEqual(deletedBeer, beer5);
+                assert.deepEqual(deletedBeer, beer4);
                 done();
             })
             .catch(done);
